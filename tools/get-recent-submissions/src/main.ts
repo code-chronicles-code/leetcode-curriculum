5aa16525bd28d9d03ea580ea0a445eaa9a89f4c4
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-06-12 13:49:27 -0700

    Migrate get submissions script to Node (#48)
    This replaces the previous Python script.

---
+++ tools/get-recent-submissions/src/main.ts
@@ -0,0 +1,17 @@
+import process from "process";
+
+import { getRecentAcSubmissionList } from "@code-chronicles/leetcode-api";
+
+async function main(): Promise<void> {
+  const recentAcSubmissionList =
+    await getRecentAcSubmissionList("VehicleOfPuzzle");
+
+  for (const submission of recentAcSubmissionList) {
+    console.log(submission);
+  }
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
