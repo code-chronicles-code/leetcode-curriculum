c202db5e9410f266d45441859d8c6ff045c50679
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-06-12 14:37:14 -0700

    Make problem list extractor write to a file (#52)
    No good reason for this. I felt like playing with temporary files.

--- tools/get-problem-list/src/main.ts
+++ tools/get-problem-list/src/main.ts
@@ -1,3 +1,6 @@
+import fsPromises, { type FileHandle } from "node:fs/promises";
+import { constants } from "fs";
+import crypto from "crypto";
 import process from "process";
 
 import {
@@ -7,6 +10,60 @@ import {
 
 // TODO: make into a shared utility?
 import { sleep } from "@code-chronicles/leetcode-api/src/sleep";
+
+export function getRandomString(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(16, (err, buf) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(buf.toString("hex"));
+      }
+    });
+  });
+}
+
+async function createTemporaryFile(
+  prefix: string = "",
+  suffix: string = "",
+): Promise<[string, FileHandle]> {
+  while (true) {
+    try {
+      const filename = [prefix, await getRandomString(), suffix].join("");
+      return [
+        filename,
+        await fsPromises.open(
+          filename,
+          constants.O_CREAT | constants.O_RDWR | constants.O_EXCL,
+        ),
+      ];
+    } catch (e) {
+      try {
+        // We won the lottery and came up with a temporary file name that
+        // already exists. Try again?
+        if ((e as { code: string }).code === "EEXIST") {
+          continue;
+        }
+      } catch {}
+
+      throw e;
+    }
+  }
+}
+
+async function writeToTemporaryFile(
+  data: Parameters<typeof fsPromises.writeFile>[1],
+  { prefix = "", suffix = "" }: { prefix?: string; suffix?: string } = {},
+): Promise<string> {
+  let fh, filename;
+  try {
+    [filename, fh] = await createTemporaryFile(prefix, suffix);
+    await fsPromises.writeFile(fh, data);
+    return filename;
+  } finally {
+    fh && (await fh.close());
+  }
+}
 
 async function main(): Promise<void> {
   let totalCount: number | null = null;
@@ -47,7 +104,12 @@ async function main(): Promise<void> {
   const problems = [...problemsMap.values()].sort(
     (a, b) => a.questionFrontendId - b.questionFrontendId,
   );
-  console.log(JSON.stringify(problems, null, 2));
+
+  const filename = await writeToTemporaryFile(
+    JSON.stringify(problems, null, 2) + "\n",
+    { prefix: "problems-", suffix: ".json" },
+  );
+  console.log(`Wrote data to: ${filename}`);
 }
 
 main().catch((err) => {
