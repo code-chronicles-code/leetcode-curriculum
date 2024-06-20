7ec075f33f2c7bcf17182899a27a3d01cae4b933
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-06-20 03:01:39 -0700

    Add a `Number.isIntegerOrIntegerObject` to the adventure pack
    I get the sense that we will need to accept integers and integer objects intechangeably in more places, so making a utility for it. The existing `Number.isInteger` returns false on objects that are integers, e.g. `new Number(5)`.

--- tools/adventure-pack/src/numberPrototypeDigits.ts
+++ tools/adventure-pack/src/numberPrototypeDigits.ts
@@ -1,3 +1,5 @@
+import "./numberIsIntegerOrIntegerObject";
+
 declare global {
   interface Number {
     digits(): Generator<number, void, undefined>;
@@ -6,26 +8,21 @@ declare global {
 }
 
 Number.prototype.digits = function (
-  this: number,
+  this: Number,
   radix: number = 10,
 ): Generator<number, void, undefined> {
-  // The Number cast is necessary for this check to work on LeetCode.
-  if (!(Number.isInteger(Number(this)) && this >= 0)) {
+  if (!(Number.isIntegerOrIntegerObject(this) && Number(this) >= 0)) {
     throw new Error("Must invoke on a non-negative integer.");
   }
-  if (!(Number.isInteger(radix) && radix >= 2)) {
+  if (!(Number.isIntegerOrIntegerObject(radix) && radix >= 2)) {
     throw new Error("Radix must be an integer >= 2.");
   }
 
-  return function* (this: number) {
-    let num = this;
+  return function* (this: Number) {
+    let num = Number(this);
     do {
       yield num % radix;
       num = Math.floor(num / radix);
     } while (num > 0);
   }.call(this);
 };
-
-// Needed to fix the error "Augmentations for the global scope can only be directly nested in external modules or ambient module declarations. ts(2669)"
-// See: https://stackoverflow.com/questions/57132428/augmentations-for-the-global-scope-can-only-be-directly-nested-in-external-modul
-export {};
