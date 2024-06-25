0638c6b5e98aa74db430af7e0c212c099b5b66bf
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-06-25 07:13:26 -0700

    Add an `Iterator.prototype.max` and an `Iterator.prototype.min` to the adventure pack (#92)
    Although spreading the iterator into `Math.max` or `Math.min` (e.g. `Math.max(...iterator)` could work for numeric types, this one will also work for strings and bigints. In a subsequent commit, we can add support for custom comparison as well!

---
+++ tools/adventure-pack/src/iteratorPrototypeMax.ts
@@ -0,0 +1,41 @@
+/**
+ * @adventure
+ * {"name": "Iterator.prototype.max"}
+ */
+
+import "./iteratorPrototypeToIterable";
+
+import { iteratorPrototype } from "./iteratorPrototype";
+
+declare global {
+  interface Iterator<T> {
+    max(): T | undefined;
+  }
+}
+
+iteratorPrototype.max = function <T>(this: Iterator<T>): T | undefined {
+  const cmp = (a: unknown, b: unknown): number => {
+    if (
+      (typeof a === "string" && typeof b === "string") ||
+      (typeof a === "number" && typeof b === "number") ||
+      (typeof a === "bigint" && typeof b === "bigint")
+    ) {
+      return a < b ? -1 : a > b ? 1 : 0;
+    }
+    throw new Error("Comparing mismatched types!");
+  };
+
+  let res: T | undefined = undefined;
+  let isFirst = true;
+  for (const element of this.toIterable()) {
+    if (
+      isFirst ||
+      cmp(element, res) > 0 ||
+      (Number.isNaN(res) && !Number.isNaN(element))
+    ) {
+      res = element;
+      isFirst = false;
+    }
+  }
+  return res;
+};
