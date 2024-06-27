50e8ebe26e9ff1e60d40b8ae81df2b34956fecea
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-06-27 03:29:05 -0700

    Adventure pack: Include recursive dependencies via topological sort

--- tools/adventure-pack/goodies/typescript/Iterator.prototype.toIterable/index.ts
+++ tools/adventure-pack/goodies/typescript/Iterator.prototype.toIterable/index.ts
@@ -14,7 +14,7 @@ declare global {
 iteratorPrototype.toIterable = function <T>(
   this: Iterator<T>,
 ): IterableIterator<T> {
-  (this as unknown as Record<string | symbol, unknown>)[Symbol.iterator] ??=
+  (this as unknown as Record<symbol, unknown>)[Symbol.iterator] ??=
     Function.returnThis;
   return this as unknown as IterableIterator<T>;
 };
