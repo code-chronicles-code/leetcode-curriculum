ecd3b49a42fd33879d8217ba83d423cde86d1aa4
Michael Volk <volkmchl@gmail.com>
2024-06-29 16:46:32 -0700

    add java folder

---
+++ tools/adventure-pack/goodies/java/UnionFind.java
@@ -0,0 +1,24 @@
+class UnionFind {
+    
+    private int[] parent;
+    
+    public UnionFind(int n){
+        parent = new int[n];
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+        }
+    }
+    
+    public void union(int a, int b){
+        int pa = find(a);
+        int pb = find(b);
+        
+        parent[pa] = parent[pb];
+    }
+    
+    public int find(int a){
+        if(parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
+    }
+    
+}
