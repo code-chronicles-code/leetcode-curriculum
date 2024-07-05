291bae781be79ab64171b28dabda0fa3ebf755e4
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-07-04 17:54:28 -0700

    Add GitHub action that posts a health report on each PR (#118)
    It's a simple API, just a list of commands that need to run successfully in the repository.

---
+++ .github/workflows/generate-health-report.js
@@ -0,0 +1,68 @@
+const { exec: execWithCallback } = require("node:child_process");
+const { promisify } = require("node:util");
+
+const exec = promisify(execWithCallback);
+
+const GITHUB_ACTIONS_BOT_ID = 41898282;
+const HEALTH_REPORT_PREFIX = `<!-- HEALTH REPORT -->\n\n`;
+
+const COMMANDS = [
+  "(cd tools && yarn format && ! (git status --porcelain | grep .))",
+  "(cd tools && yarn lint)",
+  "(cd tools && yarn typecheck)",
+  "(cd tools && yarn test)",
+  "(cd tools/adventure-pack && yarn build-app)",
+];
+
+module.exports = async ({ context, github }) => {
+  const prNumber = context.payload.pull_request.number;
+
+  const existingHealthReport = await github.rest.issues
+    .listComments({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: prNumber,
+    })
+    .then(({ data }) =>
+      data.find(
+        (c) =>
+          c.user.id === GITHUB_ACTIONS_BOT_ID &&
+          c.body.startsWith(HEALTH_REPORT_PREFIX),
+      ),
+    );
+
+  const lines = [];
+  for (const command of COMMANDS) {
+    console.log("Running: " + command);
+    try {
+      await exec(command);
+      lines.push(` * \`${command}\`: ✅`);
+    } catch (err) {
+      console.error(err);
+      lines.push(` * \`${command}\`: ❌`);
+    }
+  }
+
+  const healthReportBody =
+    HEALTH_REPORT_PREFIX +
+    "# PR Health Report\n\nLast checked commit " +
+    context.payload.pull_request.head.sha +
+    ".\n\n" +
+    lines.map((line) => line + "\n").join("");
+
+  if (existingHealthReport) {
+    await github.rest.issues.updateComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      comment_id: existingHealthReport.id,
+      body: healthReportBody,
+    });
+  } else {
+    await github.rest.issues.createComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: prNumber,
+      body: healthReportBody,
+    });
+  }
+};
