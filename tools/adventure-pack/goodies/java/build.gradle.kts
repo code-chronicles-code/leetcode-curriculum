b1d6e883bb32295ee8a1322be7da5d89d83cca46
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-07-07 10:42:20 -0700

    Use ktfmt to format Kotlin files (#134)
    It seems more actively maintained than the Prettier plugin. See: https://github.com/facebook/ktfmt

--- tools/adventure-pack/goodies/java/build.gradle.kts
+++ tools/adventure-pack/goodies/java/build.gradle.kts
@@ -1,39 +1,46 @@
+import com.ncorti.ktfmt.gradle.tasks.*
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
-    java
+  java
+
+  id("com.ncorti.ktfmt.gradle") version "0.19.0"
 }
 
 sourceSets {
-    main {
-        java {
-            srcDirs("src")
-            exclude("**/Test.java")
-        }
+  main {
+    java {
+      srcDirs("src")
+      exclude("**/Test.java")
     }
-    test {
-        java {
-            srcDirs("src")
-            include("**/Test.java")
-        }
+  }
+
+  test {
+    java {
+      srcDirs("src")
+      include("**/Test.java")
     }
+  }
 }
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.3"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  testImplementation(platform("org.junit:junit-bom:5.10.3"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
-    useJUnitPlatform()
-    testLogging {
-        events("failed", "passed", "skipped", "standard_error", "standard_out")
+  useJUnitPlatform()
+  testLogging {
+    events("failed", "passed", "skipped", "standard_error", "standard_out")
 
-        exceptionFormat = TestExceptionFormat.FULL
-    }
+    exceptionFormat = TestExceptionFormat.FULL
+  }
+}
+
+tasks.register<KtfmtFormatTask>("ktfmtGradleFiles") {
+  source = project.fileTree(rootDir)
+  include("*.gradle.kts")
 }
--- tools/adventure-pack/prettier.config.js
+++ tools/adventure-pack/prettier.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  plugins: ["prettier-plugin-java", "prettier-plugin-kotlin"],
+  plugins: ["prettier-plugin-java"],
 };
