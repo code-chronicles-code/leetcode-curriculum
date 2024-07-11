b325331fa1214b9d50868e46356325bc120c816d
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-07-11 14:35:05 -0700

    Add TypeScript goodies for traversing binary trees (#157)
    Tested on Leetcode:
    
    * Preorder: https://leetcode.com/problems/binary-tree-preorder-traversal/submissions/1316869104/
    * Inorder: https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/1316866526/
    * Postorder: https://leetcode.com/problems/binary-tree-postorder-traversal/submissions/1316867885/

---
+++ tools/adventure-pack/goodies/typescript/traversePostorder/index.ts
@@ -0,0 +1,15 @@
+export function* traversePostorder<
+  T extends { left?: T | null | undefined; right?: T | null | undefined },
+>(root: T | null | undefined): Generator<T, void, void> {
+  const stack: [T | null | undefined, boolean][] = [[root, false]];
+  while (stack.length > 0) {
+    const [node, didTraverseChildren] = stack.pop()!;
+    if (node) {
+      if (didTraverseChildren) {
+        yield node;
+      } else {
+        stack.push([node, true], [node.right, false], [node.left, false]);
+      }
+    }
+  }
+}
