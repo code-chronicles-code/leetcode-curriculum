591f04f425fbce2506a86bc518881075b968cd6f
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-07-11 14:27:51 -0700

    Rename a variable

--- tools/adventure-pack/goodies/typescript/traverseInorder/index.ts
+++ tools/adventure-pack/goodies/typescript/traverseInorder/index.ts
@@ -3,9 +3,9 @@ export function* traverseInorder<
 >(root: T | null | undefined): Generator<T, void, void> {
   const stack: [T | null | undefined, boolean][] = [[root, false]];
   while (stack.length > 0) {
-    const [node, didTraverseChildren] = stack.pop()!;
+    const [node, didTraverseLeftChild] = stack.pop()!;
     if (node) {
-      if (didTraverseChildren) {
+      if (didTraverseLeftChild) {
         yield node;
       } else {
         stack.push([node.right, false], [node, true], [node.left, false]);
