cc33c9c7b3983484b24aba066c607644523b987f
Michael Volk <volkmchl@gmail.com>
2024-07-13 18:36:57 -0700

    add componentSize optimization in union and components to UnionFind

--- tools/adventure-pack/goodies/java/src/union_find/UnionFind.java
+++ tools/adventure-pack/goodies/java/src/union_find/UnionFind.java
@@ -3,19 +3,37 @@ package union_find;
 public class UnionFind {
 
   private final int[] parent;
+  private final int[] componentSize;
+  private int components;
 
   public UnionFind(int n) {
     parent = new int[n];
+    componentSize = new int[n];
+    components = n;
     for (int i = 0; i < n; ++i) {
       parent[i] = i;
+      componentSize[i] = 1;
     }
   }
 
-  public void union(int a, int b) {
+  public boolean union(int a, int b) {
     int pa = find(a);
     int pb = find(b);
 
-    parent[pa] = parent[pb];
+    if (pa == pb) {
+      return false;
+    }
+
+    if (componentSize[pa] > componentSize[pb]) {
+      componentSize[pa] += componentSize[pb];
+      parent[pb] = parent[pa];
+    } else {
+      componentSize[pb] += componentSize[pa];
+      parent[pa] = parent[pb];
+    }
+    --components;
+
+    return true;
   }
 
   public int find(int a) {
@@ -26,7 +44,7 @@ public class UnionFind {
     return parent[a] = find(parent[a]);
   }
 
-  public boolean areConnected(int a, int b) {
-    return find(a) == find(b);
-  }
+  public boolean areConnected(int a, int b) { return find(a) == find(b); }
+
+  public int components() { return components; }
 }
