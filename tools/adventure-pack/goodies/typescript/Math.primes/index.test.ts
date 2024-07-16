903571559bcace1821dc10f9e22d135f36714c68
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-07-15 22:03:23 -0700

    Only divide by primes below the square root of the number in `Math.primes` goody (#169)

--- tools/adventure-pack/goodies/typescript/Math.primes/index.test.ts
+++ tools/adventure-pack/goodies/typescript/Math.primes/index.test.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from "@jest/globals";
 import "./index";
 
 describe("Math.primes", () => {
-  it("generates primes", () => {
+  it("generates the correct small primes", () => {
     const primes = Math.primes();
 
     [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59].forEach(
@@ -11,5 +11,19 @@ describe("Math.primes", () => {
         expect(primes.next().value).toBe(p);
       },
     );
+  });
+
+  it("generates the correct number of primes", () => {
+    let count = 0;
+
+    for (const p of Math.primes()) {
+      if (p > 10 ** 6) {
+        break;
+      }
+
+      ++count;
+    }
+
+    expect(count).toBe(78498);
   });
 });
