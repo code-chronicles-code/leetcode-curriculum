0d572b7ae207cc2642780afbfec1b5a4af1b509b
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-07-17 09:33:54 -0700

    Add `isNonNullish` TypeScript goody (#191)
    This allows filtering nulls without a type cast!

---
+++ tools/adventure-pack/goodies/typescript/isNonNullish/index.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, it } from "@jest/globals";
+
+import { isNonNullish } from "./index";
+
+describe("isNonNullish", () => {
+  it.each([[], {}, new Set(), new Map(), "hi", 4, Symbol(undefined)])(
+    "returns true for non-nullish values",
+    (value) => {
+      expect(isNonNullish(value)).toBe(true);
+    },
+  );
+
+  it("returns false for null and undefined", () => {
+    expect(isNonNullish(null)).toBe(false);
+    expect(isNonNullish(undefined)).toBe(false);
+  });
+
+  it.each([false, "", NaN, 0, -0, 0n, -0n])(
+    "returns true for non-nullish falsy value %p",
+    (value) => {
+      expect(isNonNullish(value)).toBe(true);
+    },
+  );
+});
