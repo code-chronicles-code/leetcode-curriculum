beadf07ddda5d0f3611b96680826c17436a64349
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-07-17 19:45:56 -0700

    Add `newArray` TypeScript goody for constructing multi-dimensional arrays (#197)

---
+++ tools/adventure-pack/goodies/typescript/newArray/index.test.ts
@@ -0,0 +1,114 @@
+import { describe, expect, it } from "@jest/globals";
+
+import { newArray } from "./index";
+
+describe("newArray", () => {
+  it("respects dimensions", () => {
+    expect(newArray([2, 3, 4])).toEqual([
+      [
+        [null, null, null, null],
+        [null, null, null, null],
+        [null, null, null, null],
+      ],
+      [
+        [null, null, null, null],
+        [null, null, null, null],
+        [null, null, null, null],
+      ],
+    ]);
+
+    expect(newArray([5, 2, 3, 1], 42)).toEqual([
+      [
+        [[42], [42], [42]],
+        [[42], [42], [42]],
+      ],
+      [
+        [[42], [42], [42]],
+        [[42], [42], [42]],
+      ],
+      [
+        [[42], [42], [42]],
+        [[42], [42], [42]],
+      ],
+      [
+        [[42], [42], [42]],
+        [[42], [42], [42]],
+      ],
+      [
+        [[42], [42], [42]],
+        [[42], [42], [42]],
+      ],
+    ]);
+  });
+
+  it("repeats the fill value by reference", () => {
+    const obj = {};
+    expect(newArray([5], obj).every((elem) => elem === obj)).toBe(true);
+  });
+
+  it("constructs distinct sub-arrays", () => {
+    const arr = newArray([3, 2, 4], -1);
+    arr[0][0][0] = 0;
+    arr[1][1][3] = 113;
+    arr[2][0][2] = 202;
+
+    expect(arr).toEqual([
+      [
+        [0, -1, -1, -1],
+        [-1, -1, -1, -1],
+      ],
+      [
+        [-1, -1, -1, -1],
+        [-1, -1, -1, 113],
+      ],
+      [
+        [-1, -1, 202, -1],
+        [-1, -1, -1, -1],
+      ],
+    ]);
+  });
+
+  it("can handle many dimensions", () => {
+    expect(newArray([1])).toEqual([null]);
+    expect(newArray([1, 1])).toEqual([[null]]);
+    expect(newArray([1, 1, 1])).toEqual([[[null]]]);
+    expect(newArray([1, 1, 1, 1])).toEqual([[[[null]]]]);
+    expect(newArray([1, 1, 1, 1, 1])).toEqual([[[[[null]]]]]);
+    expect(newArray([1, 1, 1, 1, 1, 1])).toEqual([[[[[[null]]]]]]);
+    expect(newArray([1, 1, 1, 1, 1, 1, 1])).toEqual([[[[[[[null]]]]]]]);
+    expect(newArray([1, 1, 1, 1, 1, 1, 1, 1])).toEqual([[[[[[[[null]]]]]]]]);
+    expect(newArray([1, 1, 1, 1, 1, 1, 1, 1, 1])).toEqual([
+      [[[[[[[[null]]]]]]]],
+    ]);
+    expect(newArray([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])).toEqual([
+      [[[[[[[[[null]]]]]]]]],
+    ]);
+
+    expect(newArray([1], 8)).toEqual([8]);
+    expect(newArray([1, 1], 8)).toEqual([[8]]);
+    expect(newArray([1, 1, 1], 8)).toEqual([[[8]]]);
+    expect(newArray([1, 1, 1, 1], 8)).toEqual([[[[8]]]]);
+    expect(newArray([1, 1, 1, 1, 1], 8)).toEqual([[[[[8]]]]]);
+    expect(newArray([1, 1, 1, 1, 1, 1], 8)).toEqual([[[[[[8]]]]]]);
+    expect(newArray([1, 1, 1, 1, 1, 1, 1], 8)).toEqual([[[[[[[8]]]]]]]);
+    expect(newArray([1, 1, 1, 1, 1, 1, 1, 1], 8)).toEqual([[[[[[[[8]]]]]]]]);
+    expect(newArray([1, 1, 1, 1, 1, 1, 1, 1, 1], 8)).toEqual([
+      [[[[[[[[8]]]]]]]],
+    ]);
+    expect(newArray([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 8)).toEqual([
+      [[[[[[[[[8]]]]]]]]],
+    ]);
+  });
+
+  it.each([Math.PI, Infinity, -Infinity, -1, NaN, 5.2])(
+    "throws with invalid dimensions [%p]",
+    (value) => {
+      expect(() => newArray([value])).toThrow();
+    },
+  );
+
+  it("throws when invoked without dimensions", () => {
+    expect(newArray).toThrow();
+    expect(() => newArray([])).toThrow();
+  });
+});
