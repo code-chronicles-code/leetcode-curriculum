05f66ca2b57c019d7744e20ea45e3d63c1f6af2e
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-07-19 22:28:09 -0700

    Get health reports to run on multiple operating systems

--- tools/adventure-pack/goodies/python3/install.sh
+++ tools/adventure-pack/goodies/python3/install.sh
@@ -5,6 +5,17 @@ set -euo pipefail
 # Change directory to the directory of the script.
 cd "$(dirname "$0")"
 
+# Prepare Python virtual environment.
 bash run_python.sh -m venv .venv
-source .venv/bin/activate
+
+# Activate Python virtual environment.
+if [ -f .venv/bin/activate ]; then
+  source .venv/bin/activate
+elif [ -f .venv/Scripts/activate ]; then
+  source .venv/Scripts/activate
+else
+  echo "Error: Could not activate the Python virtual environment." >&2
+  exit 1
+fi
+
 bash run_python.sh -m pip install -r requirements.txt
--- tools/adventure-pack/goodies/python3/test.sh
+++ tools/adventure-pack/goodies/python3/test.sh
@@ -5,5 +5,14 @@ set -euo pipefail
 # Change directory to the directory of the script.
 cd "$(dirname "$0")"
 
-source .venv/bin/activate
+# Activate Python virtual environment.
+if [ -f .venv/bin/activate ]; then
+  source .venv/bin/activate
+elif [ -f .venv/Scripts/activate ]; then
+  source .venv/Scripts/activate
+else
+  echo "Error: Could not activate the Python virtual environment." >&2
+  exit 1
+fi
+
 bash run_python.sh -m pytest --color=yes
