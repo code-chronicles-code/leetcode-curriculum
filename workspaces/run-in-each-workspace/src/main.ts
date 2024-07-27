2c21d30bfb9b66b8050b260882e03543dc9e8aef
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-07-26 18:06:37 -0700

    Turn `run-in-each-workspace` script into its own workspace (#240)
    This allows us to import other packages, convert it to TypeScript, etc.

--- workspaces/run-in-each-workspace/src/main.ts
+++ workspaces/run-in-each-workspace/src/main.ts
@@ -1,40 +1,30 @@
-const { spawn } = require("node:child_process");
-const fsPromises = require("node:fs/promises");
-const process = require("node:process");
+import process from "node:process";
+import type { IterableElement } from "type-fest";
 
-const COMMANDS = new Set(["format", "lint", "test", "typecheck"]);
+import { getCurrentGitRepositoryRoot } from "@code-chronicles/util/getCurrentGitRepositoryRoot";
+import { readWorkspaces } from "@code-chronicles/util/readWorkspaces";
+import { spawnWithSafeStdio } from "@code-chronicles/util/spawnWithSafeStdio";
+import { stripPrefixOrThrow } from "@code-chronicles/util/stripPrefixOrThrow";
 
-const SKIP = {
+// TODO: figure out a way to mark read-only
+const COMMANDS = new Set(["format", "lint", "test", "typecheck"] as const);
+
+type Command = IterableElement<typeof COMMANDS>;
+
+function isCommand(value: string): value is Command {
+  return COMMANDS.has(value as Command);
+}
+
+const SKIP: Readonly<Record<string, ReadonlySet<Command>>> = {
   "download-submissions": new Set(["test"]),
   "eslint-config": new Set(["test", "typecheck"]),
   "fetch-leetcode-problem-list": new Set(["test"]),
   "get-recent-submissions": new Set(["test"]),
   "leetcode-api": new Set(["test"]),
   "post-potd": new Set(["test"]),
+  "run-in-each-workspace": new Set(["test"]),
   util: new Set(["test"]),
 };
-
-// TODO: reusable utility!
-function runOrThrow(command, args, options = {}) {
-  return new Promise((resolve, reject) => {
-    const childProcess = spawn(command, args, {
-      ...options,
-      stdio: ["ignore", "pipe", "pipe"],
-    });
-
-    childProcess.stdout.pipe(process.stdout);
-    childProcess.stderr.pipe(process.stderr);
-
-    childProcess.on("error", reject);
-    childProcess.on("exit", (exitCode) => {
-      if (exitCode) {
-        reject(new Error(`Non-zero exit code ${exitCode}.`));
-      } else {
-        resolve();
-      }
-    });
-  });
-}
 
 async function main() {
   if (process.argv.length < 3) {
@@ -49,19 +39,17 @@ async function main() {
   }
 
   const command = process.argv[2];
-  if (!COMMANDS.has(command)) {
+  if (!isCommand(command)) {
     throw new Error(`Invalid command: ${command}`);
   }
 
-  process.chdir(__dirname);
+  process.chdir(await getCurrentGitRepositoryRoot());
 
-  const workspaceDirectories = JSON.parse(
-    await fsPromises.readFile("package.json", "utf8"),
-  ).workspaces;
+  const workspaceDirectories = await readWorkspaces("package.json");
 
   let hasError = false;
   for (const workspaceDirectory of workspaceDirectories) {
-    const workspaceName = workspaceDirectory.replace(/^workspaces\//, "");
+    const workspaceName = stripPrefixOrThrow(workspaceDirectory, "workspaces/");
     if (SKIP[workspaceName]?.has(command)) {
       console.error(
         `Skipping command ${command} for workspace ${workspaceName}`,
@@ -71,10 +59,10 @@ async function main() {
 
     try {
       // eslint-disable-next-line no-await-in-loop
-      await runOrThrow("yarn", [command], {
+      await spawnWithSafeStdio("yarn", [command], {
         cwd: workspaceDirectory,
         shell: "bash",
-        env: { ...process.env, FORCE_COLOR: 1 },
+        env: { ...process.env, FORCE_COLOR: "1" },
       });
     } catch (err) {
       hasError = true;
--- workspaces/util/src/readWorkspaces.ts
+++ workspaces/util/src/readWorkspaces.ts
@@ -1,12 +1,12 @@
 import fsPromises from "node:fs/promises";
 
-import { assertIsArray } from "@code-chronicles/util//assertIsArray";
+import { assertIsArray } from "@code-chronicles/util/assertIsArray";
 import { assertIsObject } from "@code-chronicles/util/assertIsObject";
 import { assertIsString } from "@code-chronicles/util/assertIsString";
 
 export async function readWorkspaces(path: string): Promise<string[]> {
   const packageJson = assertIsObject(
-    JSON.stringify(await fsPromises.readFile(path, "utf8")),
+    JSON.parse(await fsPromises.readFile(path, "utf8")),
   );
   const workspaces = assertIsArray(packageJson.workspaces);
   return workspaces.map(assertIsString);
