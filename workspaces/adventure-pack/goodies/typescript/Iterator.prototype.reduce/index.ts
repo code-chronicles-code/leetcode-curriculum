b86f653754e744ae567b7bcdb3b6c39d8fb5e497
Eli Manzo <96manzo.eli@gmail.com>
2024-07-30 21:23:40 -0700

    feat(jest test): Adding test for reduce

--- workspaces/adventure-pack/goodies/typescript/Iterator.prototype.reduce/index.ts
+++ workspaces/adventure-pack/goodies/typescript/Iterator.prototype.reduce/index.ts
@@ -3,29 +3,29 @@ import { iteratorPrototype } from "../Iterator.prototype";
 
 declare global {
   interface Iterator<T> {
-    reduce(
+    reduce<U>(
       this: Iterator<T>,
-      callbackFn: (accumulator: T, value: T, index: number) => T,
-      initialValue?: T,
-    ): T;
+      callbackFn: (accumulator: U, value: T, index: number) => U,
+      initialValue?: U,
+    ): U;
   }
 }
 
-iteratorPrototype.reduce ??= function <T>(
+iteratorPrototype.reduce ??= function <T extends U, U>(
   this: Iterator<T>,
-  callbackFn: (accumulator: T, value: T, index: number) => T,
-  initialValue?: T,
-): T {
-  let iterator = this.toIterable()[Symbol.iterator]();
+  callbackFn: (accumulator: U, value: T, index: number) => T,
+  initialValue?: U,
+): U {
+  const iterator = this.toIterable()[Symbol.iterator]();
   let index = 0;
-  let accumulator: T;
+  let accumulator: U;
 
   if (initialValue === undefined) {
     const firstResult = iterator.next();
     if (firstResult.done) {
       throw new TypeError("Reduce of empty iterator with no initial value");
     }
-    accumulator = firstResult.value;
+    accumulator = firstResult.value as unknown as U;
     ++index;
   } else {
     accumulator = initialValue;
