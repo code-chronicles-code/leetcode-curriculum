93999b40bb12a39584afe0558dbffc60c5d64d93
Volodymyr Semenov <Vandorlot@gmail.com>
2024-08-04 17:59:26 -0400

    Added tests and undid package.json change

--- workspaces/adventure-pack/goodies/python3/src/is_palindrome/test.py
+++ workspaces/adventure-pack/goodies/python3/src/is_palindrome/test.py
@@ -9,14 +9,24 @@ def test_odd_fail():
     assert not is_palindrome("1234221")
 
 
-def test_pass():
+def test_odd_pass():
     assert is_palindrome("palindromemordnilap")
 
 
-def test_arr():
+def test_even_pass():
+    assert is_palindrome("palindromeemordnilap")
+
+
+def test_list():
     assert not is_palindrome([1, 1, 2, 3, 5, 2, 1, 1])
 
 
-def test_single_char():
+def test_single_element():
     assert is_palindrome("#")
     assert is_palindrome("a")
+    assert is_palindrome(["a"])
+
+
+def test_empty_element():
+    assert is_palindrome("")
+    assert is_palindrome([])
