1e7f9fb77714b502119f3c44080d688400e6d598
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-08-04 12:02:44 -0700

    Add a build option for Post PotD package
    The built script errors due to a WebSocket error. But we'll fix that later.

--- workspaces/util/src/sleep.ts
+++ workspaces/util/src/sleep.ts
@@ -1,3 +1,5 @@
+// TODO: consider node:timers/promises instead
+
 export function sleep(milliseconds: number): Promise<void> {
   return new Promise((resolve) => {
     setTimeout(resolve, milliseconds);
