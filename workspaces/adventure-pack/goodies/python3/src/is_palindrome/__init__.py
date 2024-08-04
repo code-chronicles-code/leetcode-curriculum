c54fe90a8080a32aed37cbeb8c9090f177b77c9b
Volodymyr Semenov <Vandorlot@gmail.com>
2024-08-04 17:30:01 -0400

    Renamed confusing parameter

--- workspaces/adventure-pack/goodies/python3/src/is_palindrome/__init__.py
+++ workspaces/adventure-pack/goodies/python3/src/is_palindrome/__init__.py
@@ -1,8 +1,8 @@
-def is_palindrome(arr: list | str) -> bool:
+def is_palindrome(sequence: list | str) -> bool:
     left: int = 0
-    right: int = len(arr) - 1
+    right: int = len(sequence) - 1
     while left < right:
-        if arr[left] != arr[right]:
+        if sequence[left] != sequence[right]:
             return False
         left += 1
         right -= 1
