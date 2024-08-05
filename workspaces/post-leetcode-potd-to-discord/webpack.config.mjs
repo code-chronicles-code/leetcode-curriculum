c510dd39911eadc74d5a2381ab03ac22ac2dcf1a
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-08-05 15:19:17 -0700

    Simplify how we externalize Node modules in webpack configs (#281)
    Per https://webpack.js.org/configuration/externals/#function

--- workspaces/post-leetcode-potd-to-discord/webpack.config.mjs
+++ workspaces/post-leetcode-potd-to-discord/webpack.config.mjs
@@ -1,3 +1,4 @@
+import { builtinModules } from "node:module";
 import path from "node:path";
 
 import webpack from "webpack";
@@ -34,47 +35,18 @@ export default {
   },
 
   externalsType: "commonjs",
-  externals: {
-    // TODO: figure out a way to programmatically generate this list or configure Node packages
-    buffer: "buffer",
-    "node:assert": "node:assert",
-    "node:async_hooks": "node:async_hooks",
-    "node:buffer": "node:buffer",
-    "node:child_process": "node:child_process",
-    "node:console": "node:console",
-    "node:crypto": "node:crypto",
-    "node:diagnostics_channel": "node:diagnostics_channel",
-    "node:events": "node:events",
-    "node:fs": "node:fs",
-    "node:fs/promises": "node:fs/promises",
-    "node:http": "node:http",
-    "node:http2": "node:http2",
-    "node:net": "node:net",
-    "node:os": "node:os",
-    "node:path": "node:path",
-    "node:perf_hooks": "node:perf_hooks",
-    "node:process": "node:process",
-    "node:querystring": "node:querystring",
-    "node:stream": "node:stream",
-    "node:timers": "node:timers",
-    "node:timers/promises": "node:timers/promises",
-    "node:tls": "node:tls",
-    "node:url": "node:url",
-    "node:util": "node:util",
-    "node:util/types": "node:util/types",
-    "node:worker_threads": "node:worker_threads",
-    "node:zlib": "node:zlib",
-    path: "path",
-    process: "process",
-    string_decoder: "string_decoder",
-    timers: "timers",
-    "timers/promises": "timers/promises",
-    worker_threads: "worker_threads",
-    url: "url",
-    util: "util",
-    zlib: "zlib",
-    "zlib-sync": "zlib-sync",
-  },
+  externals: [
+    async ({ request }) =>
+      builtinModules.includes(request) ||
+      builtinModules.includes(request.replace(/^node:/, ""))
+        ? request
+        : undefined,
+
+    {
+      // TODO: see if we can use asset/inline for this library
+      "zlib-sync": "zlib-sync",
+    },
+  ],
 
   plugins: [
     new webpack.BannerPlugin({
