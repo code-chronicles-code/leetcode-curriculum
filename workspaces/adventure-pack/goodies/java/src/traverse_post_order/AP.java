daa69003c50183ad3e01ac7282d94b74161b380b
Utsav Prashant Tayde <Utsav>
2024-08-08 00:10:04 +0530

    post order traversal java goodie

---
+++ workspaces/adventure-pack/goodies/java/src/traverse_post_order/AP.java
@@ -0,0 +1,68 @@
+package traverse_post_order;
+
+import iterable_stream.IterableStream;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Stack;
+import pair.Pair;
+import simple_iterator.SimpleIterator;
+
+/**
+ * Post-order binary tree traversal implementation.
+ */
+public final class AP {
+
+  /**
+   * Get {@link IterableStream iterable stream} of all the {@link TreeNode nodes} in the binary tree
+   * in post-order.
+   * @param root Binary tree root {@link TreeNode node}.
+   * @return The {@link IterableStream iterable stream} in post-order.
+   */
+  public static IterableStream<TreeNode> traversePostOrder(
+    final TreeNode root
+  ) {
+    final Stack<Pair<TreeNode, Boolean>> stack = new Stack<
+      Pair<TreeNode, Boolean>
+    >();
+    stack.push(new Pair<>(root, false));
+    return IterableStream.from(
+      SimpleIterator.toIterator(() -> {
+        while (!stack.isEmpty()) {
+          Pair<TreeNode, Boolean> peek = stack.pop();
+          TreeNode<T> node = peek.first();
+          boolean didTraverseChildren = peek.second();
+          if (node == null) continue;
+          if (didTraverseChildren) {
+            return node.val;
+          }
+          stack.push(new Pair(node, true));
+          stack.push(new Pair(node.right, false));
+          stack.push(new Pair(node.left, false));
+        }
+        throw new NoSuchElementException();
+      })
+    );
+  }
+}
+
+/**
+ * Class representing the a single node in the binary tree.
+ */
+class TreeNode {
+
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
