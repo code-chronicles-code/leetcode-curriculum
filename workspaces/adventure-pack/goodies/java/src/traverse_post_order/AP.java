b173efa26450ab0fb69f86598ce1eade223a7983
Utsav <113458845+utsavatwork@users.noreply.github.com>
2024-08-10 23:51:39 +0530

    Post order traversal Java goody (#291)
    Addresses #173.

---
+++ workspaces/adventure-pack/goodies/java/src/traverse_post_order/AP.java
@@ -0,0 +1,67 @@
+package traverse_post_order;
+
+import iterable_stream.IterableStream;
+import java.util.NoSuchElementException;
+import java.util.Stack;
+import pair.Pair;
+import simple_iterator.SimpleIterator;
+
+public final class AP {
+
+  /**
+   * Get an {@link IterableStream iterable stream} of all the {@link TreeNode nodes} in the binary
+   * tree in post-order.
+   * @param root Binary tree root {@link TreeNode node}.
+   * @return The {@link IterableStream iterable stream} in post-order.
+   */
+  public static IterableStream<TreeNode> traversePostOrder(
+    final TreeNode root
+  ) {
+    final var stack = new Stack<Pair<TreeNode, Boolean>>();
+    stack.push(new Pair<>(root, false));
+
+    return IterableStream.from(
+      SimpleIterator.toIterator(() -> {
+        while (!stack.isEmpty()) {
+          var nodeAndDidTraverseChildren = stack.pop();
+
+          var node = nodeAndDidTraverseChildren.first();
+          if (node == null) {
+            continue;
+          }
+
+          if (nodeAndDidTraverseChildren.second()) {
+            return node;
+          }
+
+          stack.push(new Pair<>(node, true));
+          stack.push(new Pair<>(node.right, false));
+          stack.push(new Pair<>(node.left, false));
+        }
+
+        throw new NoSuchElementException();
+      })
+    );
+  }
+}
+
+class TreeNode {
+
+  // Class representing a single node in the binary tree.
+
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
