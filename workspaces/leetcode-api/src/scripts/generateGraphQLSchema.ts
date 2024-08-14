88aaa262af73cc55a9efbd9612177c85d305d050
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-08-13 19:06:14 -0700

    LeetCode GraphQL: Generate type/interfaces declarations (#320)

--- workspaces/leetcode-api/src/scripts/generateGraphQLSchema.ts
+++ workspaces/leetcode-api/src/scripts/generateGraphQLSchema.ts
@@ -1,12 +1,17 @@
-import fsPromises from "node:fs/promises";
+import { readdir as readDirectory, readFile } from "node:fs/promises";
 import path from "node:path";
 
 import nullthrows from "nullthrows";
 
-import type { LeetCodeGraphQLType } from "../fetchGraphQLTypeInformation";
+import { isStringEmptyOrWhitespaceOnly } from "@code-chronicles/util/isStringEmptyOrWhitespaceOnly";
+
+import type {
+  InnerType,
+  LeetCodeGraphQLType,
+} from "../fetchGraphQLTypeInformation";
 
 function formatDescription(description: string | undefined): string {
-  if (description == null) {
+  if (description == null || isStringEmptyOrWhitespaceOnly(description)) {
     return "";
   }
 
@@ -15,13 +20,27 @@ function formatDescription(description: string | undefined): string {
     : `"${description}"\n`;
 }
 
+function stringifyType(innerType: InnerType): string {
+  if (innerType.kind === "LIST") {
+    return "[" + stringifyType(nullthrows(innerType.ofType)) + "]";
+  }
+
+  if (innerType.kind === "NON_NULL") {
+    return stringifyType(nullthrows(innerType.ofType)) + "!";
+  }
+
+  return nullthrows(innerType.name);
+}
+
 async function main(): Promise<void> {
-  const fileEntries = await fsPromises.readdir("types", {
+  const fileEntries = await readDirectory("types", {
     withFileTypes: true,
   });
 
   const scalars: string[] = [];
   const enums: string[] = [];
+  const interfaces: string[] = [];
+  const objects: string[] = [];
 
   for (const entry of fileEntries) {
     if (!(entry.isFile() && entry.name.endsWith(".json"))) {
@@ -30,23 +49,45 @@ async function main(): Promise<void> {
 
     const graphqlTypeInfo = JSON.parse(
       // eslint-disable-next-line no-await-in-loop
-      await fsPromises.readFile(path.join("types", entry.name), "utf8"),
+      await readFile(path.join("types", entry.name), "utf8"),
     ) as LeetCodeGraphQLType;
-    if (graphqlTypeInfo.kind === "SCALAR") {
-      scalars.push(
-        `${formatDescription(graphqlTypeInfo.description)}scalar ${graphqlTypeInfo.name}\n`,
-      );
-    }
 
-    if (graphqlTypeInfo.kind === "ENUM") {
-      const enumValues = nullthrows(graphqlTypeInfo.enumValues).map(
-        (ev) => `${formatDescription(ev.description)}${ev.name}\n`,
-      );
-      enums.push(`enum ${graphqlTypeInfo.name} {\n${enumValues.join("")}}\n`);
+    // TODO: handle all the kind types
+    switch (graphqlTypeInfo.kind) {
+      case "ENUM": {
+        const enumValues = nullthrows(graphqlTypeInfo.enumValues).map(
+          (ev) => `${formatDescription(ev.description)}${ev.name}\n`,
+        );
+        enums.push(
+          `${formatDescription(graphqlTypeInfo.description)}enum ${graphqlTypeInfo.name} {\n${enumValues.join("")}}\n`,
+        );
+        break;
+      }
+      case "INTERFACE":
+      case "OBJECT": {
+        const decl =
+          graphqlTypeInfo.kind === "INTERFACE" ? "interface" : "type";
+        const fields = nullthrows(graphqlTypeInfo.fields).map(
+          (field) =>
+            `${formatDescription(field.description)}${field.name}: ${stringifyType(field.type)}\n`,
+        );
+
+        // TODO: also include field arguments
+        (graphqlTypeInfo.kind === "INTERFACE" ? interfaces : objects).push(
+          `${formatDescription(graphqlTypeInfo.description)}${decl} ${graphqlTypeInfo.name} {\n${fields.join("")}}\n`,
+        );
+        break;
+      }
+      case "SCALAR": {
+        scalars.push(
+          `${formatDescription(graphqlTypeInfo.description)}scalar ${graphqlTypeInfo.name}\n`,
+        );
+        break;
+      }
     }
   }
 
-  console.log([...scalars, ...enums].join("\n"));
+  console.log([...scalars, ...enums, ...interfaces, ...objects].join("\n"));
 }
 
 main().catch((err) => {
