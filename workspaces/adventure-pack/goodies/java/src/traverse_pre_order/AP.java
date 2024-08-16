bfd2078ca0b15d425692719d59303db8c4804671
Utsav <113458845+utsavatwork@users.noreply.github.com>
2024-08-16 13:36:43 +0530

    Binary tree preorder traversal Java goody (#329)
    Closes #172.

---
+++ workspaces/adventure-pack/goodies/java/src/traverse_pre_order/AP.java
@@ -0,0 +1,57 @@
+package traverse_pre_order;
+
+import iterable_stream.IterableStream;
+import java.util.NoSuchElementException;
+import java.util.Stack;
+import simple_iterator.SimpleIterator;
+
+public final class AP {
+
+  /**
+   * Get an {@link IterableStream iterable stream} of all the {@link TreeNode nodes} in the binary
+   * tree in pre-order.
+   * @param root Binary tree root {@link TreeNode node}.
+   * @return The {@link IterableStream iterable stream} in pre-order.
+   */
+  public static IterableStream<TreeNode> traversePreOrder(final TreeNode root) {
+    final var stack = new Stack<TreeNode>();
+    stack.push(root);
+
+    return IterableStream.from(
+      SimpleIterator.toIterator(() -> {
+        while (!stack.isEmpty()) {
+          var node = stack.pop();
+
+          if (node != null) {
+            stack.push(node.right);
+            stack.push(node.left);
+            return node;
+          }
+        }
+
+        throw new NoSuchElementException();
+      })
+    );
+  }
+}
+
+class TreeNode {
+
+  // Class representing a single node in the binary tree.
+
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {}
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
