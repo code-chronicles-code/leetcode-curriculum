09c1cdca9388b1b9c100f541d174893c5a3b5244
Michael Miller <miller.t.michael@hotmail.com>
2024-08-16 02:17:41 -0600

    Add copy button to HighlightedCode component (#324)
    Closes #152.

--- workspaces/adventure-pack/src/app/components/HighlightedCode.tsx
+++ workspaces/adventure-pack/src/app/components/HighlightedCode.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { tomorrow as syntaxHighlighterStyle } from "react-syntax-highlighter/dist/esm/styles/prism";
-
+import { CopyButton } from "./CopyButton";
 import type { Language } from "../Language";
 
 // Cross-reference with https://github.com/react-syntax-highlighter/react-syntax-highlighter/blob/master/AVAILABLE_LANGUAGES_PRISM.MD
@@ -22,20 +22,23 @@ const LANGUAGE_TO_HIGHLIGHTER_LANGUAGE: Record<Language, HighlighterLanguage> =
   };
 
 type Props = {
-  children: React.ComponentProps<typeof SyntaxHighlighter>["children"];
+  children: string;
 
   language: Language;
 };
 
 export function HighlightedCode({ children, language }: Props) {
   return (
-    <SyntaxHighlighter
-      customStyle={{ fontSize: "12px", margin: 0 }}
-      language={LANGUAGE_TO_HIGHLIGHTER_LANGUAGE[language]}
-      showLineNumbers
-      style={syntaxHighlighterStyle}
-    >
-      {children}
-    </SyntaxHighlighter>
+    <div className="highlighted-code">
+      <CopyButton textToCopy={children} />
+      <SyntaxHighlighter
+        customStyle={{ fontSize: "12px", margin: 0 }}
+        language={LANGUAGE_TO_HIGHLIGHTER_LANGUAGE[language]}
+        showLineNumbers
+        style={syntaxHighlighterStyle}
+      >
+        {children}
+      </SyntaxHighlighter>
+    </div>
   );
 }
