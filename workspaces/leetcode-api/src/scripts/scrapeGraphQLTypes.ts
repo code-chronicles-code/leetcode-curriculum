fffab5efa68609db3b5298f5a4ced69d765a4ffe
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-08-17 22:46:13 -0700

    LeetCode GraphQL: scrape types in larger batches (#333)
    This makes rescraping much faster!

--- workspaces/leetcode-api/src/scripts/scrapeGraphQLTypes.ts
+++ workspaces/leetcode-api/src/scripts/scrapeGraphQLTypes.ts
@@ -2,14 +2,16 @@ import { writeFile } from "node:fs/promises";
 import path from "node:path";
 
 import invariant from "invariant";
-import nullthrows from "nullthrows";
 
+import { popMany } from "@code-chronicles/util/popMany";
 import { sleep } from "@code-chronicles/util/sleep";
 
 import {
   fetchGraphQLTypeInformation,
   type InnerType,
 } from "../fetchGraphQLTypeInformation";
+
+const BATCH_SIZE = 25;
 
 async function main(): Promise<void> {
   const stack: string[] = [];
@@ -42,43 +44,49 @@ async function main(): Promise<void> {
     .forEach(pushTypeName);
 
   while (stack.length > 0) {
-    const typeName = nullthrows(stack.pop());
-    console.log(`Fetching ${typeName}, ${stack.length} to go`);
+    const typeNames = popMany(stack, BATCH_SIZE);
+    console.log(`Fetching ${typeNames.join(", ")}, ${stack.length} to go`);
 
     // eslint-disable-next-line no-await-in-loop
-    const graphqlTypeInfo = await fetchGraphQLTypeInformation(typeName);
-    if (graphqlTypeInfo == null) {
-      invariant(
-        typeName === "Subscription",
-        "Subscription is the only type we currently expect to not be defined.",
-      );
-      continue;
-    }
+    const graphqlTypeInfos = await fetchGraphQLTypeInformation(typeNames);
 
     // eslint-disable-next-line no-await-in-loop
-    await writeFile(
-      path.join("types", `${typeName}.json`),
-      JSON.stringify(graphqlTypeInfo, null, 2) + "\n",
+    await Promise.all(
+      typeNames.map(async (typeName) => {
+        const graphqlTypeInfo = graphqlTypeInfos[typeName];
+        if (graphqlTypeInfo == null) {
+          invariant(
+            typeName === "Subscription",
+            "Subscription is the only type we currently expect to not be defined.",
+          );
+          return;
+        }
+
+        await writeFile(
+          path.join("types", `${typeName}.json`),
+          JSON.stringify(graphqlTypeInfo, null, 2) + "\n",
+        );
+
+        for (const field of graphqlTypeInfo.fields ?? []) {
+          pushType(field.type);
+          for (const arg of field.args ?? []) {
+            pushType(arg.type);
+          }
+        }
+
+        for (const inputField of graphqlTypeInfo.inputFields ?? []) {
+          pushType(inputField.type);
+        }
+
+        for (const iface of graphqlTypeInfo.interfaces ?? []) {
+          pushType(iface);
+        }
+
+        for (const possibleType of graphqlTypeInfo.possibleTypes ?? []) {
+          pushType(possibleType);
+        }
+      }),
     );
-
-    for (const field of graphqlTypeInfo.fields ?? []) {
-      pushType(field.type);
-      for (const arg of field.args ?? []) {
-        pushType(arg.type);
-      }
-    }
-
-    for (const inputField of graphqlTypeInfo.inputFields ?? []) {
-      pushType(inputField.type);
-    }
-
-    for (const iface of graphqlTypeInfo.interfaces ?? []) {
-      pushType(iface);
-    }
-
-    for (const possibleType of graphqlTypeInfo.possibleTypes ?? []) {
-      pushType(possibleType);
-    }
 
     // eslint-disable-next-line no-await-in-loop
     await sleep(3000);
