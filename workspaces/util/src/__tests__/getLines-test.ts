22543ec9f7159727c3f393c824fea611caabea50
Mahmood Alhawaj <119938418+Ma-hawaj@users.noreply.github.com>
2024-08-21 13:15:02 +0300

    added tests for getline utility

---
+++ workspaces/util/src/__tests__/getLines-test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, it } from "@jest/globals";
+
+import { getLines } from "../getLines";
+
+describe("chunkBySize", () => {
+
+  it("gets lines when the new line at the begining", () => {
+    const result = getLines("\n hello\n world");
+
+    expect([...result]).toStrictEqual(["\n", " hello\n", " world"]);
+
+  });
+
+  it("gets lines when the new line at the end", () => {
+    const result = getLines("hello\nworld\n!!\n");
+
+    expect([...result]).toStrictEqual(["hello\n", "world\n", "!!\n"]);
+
+  });
+
+  it("gets empty array when empty string is given", () => {
+    const result = getLines("");
+
+    expect([...result]).toStrictEqual([]);
+
+  });
+
+  it("gets a single line when a single line provided", () => {
+    const result = getLines("hello world !");
+
+    expect([...result]).toStrictEqual(["hello world !"]);
+
+  });
+
+  it("gets lines when using other new line characters", () => {
+    const result = getLines("this\vis\fa\rtest");
+
+    expect([...result]).toStrictEqual(["this\v","is\f","a\r","test"]);
+
+  });
+
+});
