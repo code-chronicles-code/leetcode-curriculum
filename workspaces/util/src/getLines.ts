1c32c988d3cd01c5bfa50d11fbb71a7c1d48db74
Mahmood Alhawaj <119938418+Ma-hawaj@users.noreply.github.com>
2024-08-21 13:18:04 +0300

    formatting

--- workspaces/util/src/getLines.ts
+++ workspaces/util/src/getLines.ts
@@ -1,5 +1,7 @@
 export function* getLines(text: string): Generator<string, void, void> {
-  for (const [line] of text.matchAll(/[^\n\v\f\r]+(\n|\r|\v|\f)?|(\n|\r|\v|\f)/g)) {
+  for (const [line] of text.matchAll(
+    /[^\n\v\f\r]+(\n|\r|\v|\f)?|(\n|\r|\v|\f)/g,
+  )) {
     yield line;
   }
 }
