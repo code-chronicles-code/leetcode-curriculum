2ebdf9a284111c5efde3271dfff9d481674b6dd5
Michael Miller <mtmiller514@gmail.com>
2024-08-21 21:28:22 -0600

    Add TypeScript `Map.groupBy` goody (#358)
    Closes #342.

---
+++ workspaces/adventure-pack/goodies/typescript/Map.groupBy/index.ts
@@ -0,0 +1,31 @@
+declare global {
+  interface MapConstructor {
+    groupBy<K, V>(
+      iterable: Iterable<V>,
+      callbackFn: (value: V, index: number) => K,
+    ): Map<K, V[]>;
+  }
+}
+
+Map.groupBy ??= function <K, V>(
+  iterable: Iterable<V>,
+  callbackFn: (value: V, index: number) => K,
+): Map<K, V[]> {
+  const groups = new Map<K, V[]>();
+  let index = 0;
+  for (const value of iterable) {
+    const key = callbackFn(value, index);
+    const group = groups.get(key);
+    if (group == null) {
+      groups.set(key, [value]);
+    } else {
+      group.push(value);
+    }
+    ++index;
+  }
+  return groups;
+};
+
+// Needed to fix the error "Augmentations for the global scope can only be directly nested in external modules or ambient module declarations. ts(2669)"
+// See: https://stackoverflow.com/questions/57132428/augmentations-for-the-global-scope-can-only-be-directly-nested-in-external-modul
+export {};
