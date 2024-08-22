58189941e2ddb952fc55228bccde34393e7ef813
Volodymyr Semenov <Vandorlot@gmail.com>
2024-08-22 14:48:52 -0400

    Added test for a list of various types

--- workspaces/adventure-pack/goodies/python3/src/flatten/test.py
+++ workspaces/adventure-pack/goodies/python3/src/flatten/test.py
@@ -9,9 +9,11 @@ def test_flatten():
         assert i == flattened[i]
 
 
-def test_flatten_not_sorted():
-    l = [[12, 11, 10, 9], [[8, 7, 6], [5, 4, 3]], 2, 1, 0]
+def test_flatten_random_types():
+    dictionary = {"something": "something_e;se"}
+    l = [["hi", -80, "c", 0.0], [[(3, 3), dictionary, 6], [5, 4, 3]], 2, 1, 0]
     flattened = list(flatten(l))
     assert isinstance(l[0], list)
+    expected = ["hi", -80, "c", 0.0, (3, 3), dictionary, 6, 5, 4, 3, 2, 1, 0]
     for i in range(len(flattened)):
-        assert flattened[i] == len(flattened) - 1 - i
+        assert flattened[i] == expected[i]
