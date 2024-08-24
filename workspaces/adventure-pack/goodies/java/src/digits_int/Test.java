57163be911cad443c882aa052406147bfca03f00
Utsav Prashant Tayde <Utsav>
2024-08-24 17:04:38 +0530

    Removing explicit exceptions from digits method and other enhancements in unit tests

--- workspaces/adventure-pack/goodies/java/src/digits_int/Test.java
+++ workspaces/adventure-pack/goodies/java/src/digits_int/Test.java
@@ -1,5 +1,6 @@
 package digits_int;
 
+import static digits_int.AP.digits;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,60 +14,37 @@ class DigitsIntTest {
 
   @Test
   public void withIntegerGreaterThanZero() {
-    try {
-      var digits = AP.digits(4984);
-      assertIterableEquals(
-        IterableIntStream.from(IntStream.of(4, 8, 9, 4)),
-        digits,
-        "Test Failed. Digits generated for positive integer are not correct."
-      );
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail();
-    }
+    var digits = digits(4984);
+    assertIterableEquals(
+      IterableIntStream.from(IntStream.of(4, 8, 9, 4)),
+      digits
+    );
   }
 
   @Test
   public void withZero() {
-    try {
-      var digits = AP.digits(0);
-      assertIterableEquals(
-        IterableIntStream.from(IntStream.of(0)),
-        digits,
-        "Test Failed. Digits generated for integer 0 are not correct."
-      );
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail();
-    }
+    var digits = digits(0);
+    assertIterableEquals(IterableIntStream.from(IntStream.of(0)), digits);
   }
 
   @Test
   public void withRadixOtherThan10() {
-    try {
-      var digits = AP.digits(12345, 7);
-      assertIterableEquals(
-        IterableIntStream.from(IntStream.of(4, 6, 6, 0, 5)),
-        digits,
-        "Test Failed. Digits generated for the integer with radix other than 10 are not correct."
-      );
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail();
-    }
+    var digits = digits(12345, 7);
+    assertIterableEquals(
+      IterableIntStream.from(IntStream.of(4, 6, 6, 0, 5)),
+      digits
+    );
   }
 
   @Test
   public void withRadixLessThan2() {
-    Exception thrown = assertThrows(
-      Exception.class,
-      () -> AP.digits(12345, 1),
-      "Test Failed. Does not throw exception for radix less than 2."
-    );
-    assertTrue(
-      thrown
-        .getMessage()
-        .contains("Please provide the radix greater than or equal to 2.")
-    );
+    Exception thrown = assertThrows(Exception.class, () -> digits(12345, 1));
+    assertTrue(thrown instanceof IllegalArgumentException);
+  }
+
+  @Test
+  public void withNegativeInteger() {
+    Exception thrown = assertThrows(Exception.class, () -> digits(-145));
+    assertTrue(thrown instanceof IllegalArgumentException);
   }
 }
