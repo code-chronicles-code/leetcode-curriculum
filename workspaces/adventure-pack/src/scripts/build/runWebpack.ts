5097e49a4fbb4849125f5c72e60814f460ec0406
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-08-24 16:36:04 -0700

    Try to figure out what's happening with the PATH in the environment

--- workspaces/adventure-pack/src/scripts/build/runWebpack.ts
+++ workspaces/adventure-pack/src/scripts/build/runWebpack.ts
@@ -1,5 +1,12 @@
 import { spawnWithSafeStdio } from "@code-chronicles/util/spawnWithSafeStdio";
 
 export async function runWebpack(): Promise<void> {
+  console.log("ENV with bash");
+  await spawnWithSafeStdio("env", [], { shell: "bash" });
+  console.log("ENV without bash");
+  await spawnWithSafeStdio("env", []);
+  console.log();
+  console.log();
+
   await spawnWithSafeStdio("webpack", ["--color"], { shell: "bash" });
 }
