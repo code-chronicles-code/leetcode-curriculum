36c7feaf42d12ccdc653212c7529fa8fd2bce614
Will Madison <wmadisonDev@GMail.com>
2024-08-24 04:03:38 -0400

    Add level order traversal Kotlin goody (#369)
    Closes #314.

---
+++ workspaces/adventure-pack/goodies/kotlin/src/traverse_level_order/Main.kt
@@ -0,0 +1,19 @@
+package traverse_level_order
+
+public data class TreeNode(val `val`: Int, var left: TreeNode? = null, var right: TreeNode? = null)
+
+public fun TreeNode?.traverseLevelOrder(): Sequence<Collection<TreeNode>> {
+  if (this == null) {
+    return sequenceOf()
+  }
+
+  var nodesAtLevel = listOf(this)
+
+  return sequence {
+    while (nodesAtLevel.isNotEmpty()) {
+      yield(nodesAtLevel)
+
+      nodesAtLevel = nodesAtLevel.flatMap { listOf(it.left, it.right) }.filterNotNull()
+    }
+  }
+}
---
+++ workspaces/adventure-pack/goodies/kotlin/src/traverse_level_order/Test.kt
@@ -0,0 +1,32 @@
+package traverse_level_order
+
+import kotlin.test.*
+
+internal class LevelOrderTraversalTest {
+
+  @Test
+  fun itTraversesLevelOrderEmptyTree() {
+    val root: TreeNode? = null
+    assertContentEquals(
+        root.traverseLevelOrder().flatMap { it }.map { it.`val` }.toList(), emptyList<Int>())
+  }
+
+  @Test
+  fun itTraversesLevelOrderSingleNode() {
+    val root: TreeNode = TreeNode(0)
+    assertContentEquals(
+        root.traverseLevelOrder().flatMap { it }.map { it.`val` }.toList(), listOf<Int>(0))
+  }
+
+  @Test
+  fun itTraversesLevelOrderFullTree() {
+    val root: TreeNode =
+        TreeNode(
+            1,
+            TreeNode(2, TreeNode(4, TreeNode(8)), TreeNode(5)),
+            TreeNode(3, TreeNode(6), TreeNode(7)))
+    assertContentEquals(
+        root.traverseLevelOrder().flatMap { it }.map { it.`val` }.toList(),
+        listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8))
+  }
+}
