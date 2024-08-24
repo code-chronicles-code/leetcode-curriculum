4dda559a20fc6422358d3776f9975b0d7b40a4c7
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-08-24 01:37:38 -0700

    Simplify `Number.prototype.digits` goody

--- workspaces/adventure-pack/goodies/typescript/Number.prototype.digits/index.ts
+++ workspaces/adventure-pack/goodies/typescript/Number.prototype.digits/index.ts
@@ -1,5 +1,3 @@
-import "../Number.isIntegerOrIntegerObject";
-
 declare global {
   interface Number {
     digits(this: Number): Generator<number, void, void>;
@@ -11,18 +9,22 @@ Number.prototype.digits = function (
   this: Number,
   radix: number = 10,
 ): Generator<number, void, void> {
-  if (!(Number.isIntegerOrIntegerObject(this) && Number(this) >= 0)) {
+  let num = Number(this);
+  if (!(Number.isInteger(num) && num >= 0)) {
     throw new Error("Must invoke on a non-negative integer.");
   }
-  if (!(Number.isIntegerOrIntegerObject(radix) && radix >= 2)) {
+  if (!(Number.isInteger(radix) && radix >= 2)) {
     throw new Error("Radix must be an integer >= 2.");
   }
 
-  return function* (this: Number) {
-    let num = Number(this);
+  return (function* () {
     do {
       yield num % radix;
       num = Math.floor(num / radix);
     } while (num > 0);
-  }.call(this);
+  })();
 };
+
+// Needed to fix the error "Augmentations for the global scope can only be directly nested in external modules or ambient module declarations. ts(2669)"
+// See: https://stackoverflow.com/questions/57132428/augmentations-for-the-global-scope-can-only-be-directly-nested-in-external-modul
+export {};
