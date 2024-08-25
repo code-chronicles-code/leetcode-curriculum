1dd4e9cfc137ec879fddc5fae829930f673066b3
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-08-24 18:40:24 -0700

    Pretty print `goodies.json` if `NODE_ENV` is "development"

--- workspaces/adventure-pack/src/scripts/build/writeGoodiesJson.ts
+++ workspaces/adventure-pack/src/scripts/build/writeGoodiesJson.ts
@@ -1,15 +1,28 @@
 import { writeFile } from "node:fs/promises";
 import path from "node:path";
 
+import prettier from "prettier";
+
+import { isEnvironmentDev } from "@code-chronicles/util/isEnvironmentDev";
+
 import { readAllGoodies } from "../package-goodies/readAllGoodies";
 import { WEBAPP_DIST } from "./constants";
 
-export async function writeGoodiesJson(): Promise<void> {
+async function readAllGoodiesAsString(): Promise<string> {
   const goodies = await readAllGoodies();
+  const text = JSON.stringify(goodies);
 
-  await writeFile(
-    path.join(WEBAPP_DIST, "goodies.json"),
-    // TODO: pretty print if NODE_ENV is "development"
-    JSON.stringify(goodies) + "\n",
-  );
+  if (isEnvironmentDev()) {
+    // Could also change the arguments to `JSON.stringify` but thought we
+    // could give Prettier the chance to do something fancier.
+    return await prettier.format(text, { parser: "json" });
+  }
+
+  return text + "\n";
+}
+
+export async function writeGoodiesJson(): Promise<void> {
+  const text = await readAllGoodiesAsString();
+
+  await writeFile(path.join(WEBAPP_DIST, "goodies.json"), text);
 }
