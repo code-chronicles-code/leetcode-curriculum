fc5cc840cf33844d0da8d14ccc04bff75bcfcb5d
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-08-25 20:38:06 -0700

    Add `traversePostOrderNAry` TypeScript goody (#388)
    Tested on LeetCode: https://leetcode.com/problems/n-ary-tree-postorder-traversal/submissions/1368487878/

--- workspaces/adventure-pack/goodies/typescript/traversePostOrder/index.ts
+++ workspaces/adventure-pack/goodies/typescript/traversePostOrder/index.ts
@@ -2,14 +2,18 @@ export function* traversePostOrder<
   T extends { left?: T | null | undefined; right?: T | null | undefined },
 >(root: T | null | undefined): Generator<T, void, void> {
   const stack: [T | null | undefined, boolean][] = [[root, false]];
-  while (stack.length > 0) {
+
+  do {
     const [node, didTraverseChildren] = stack.pop()!;
-    if (node) {
-      if (didTraverseChildren) {
-        yield node;
-      } else {
-        stack.push([node, true], [node.right, false], [node.left, false]);
-      }
+    if (node == null) {
+      continue;
     }
-  }
+
+    if (didTraverseChildren) {
+      yield node;
+      continue;
+    }
+
+    stack.push([node, true], [node.right, false], [node.left, false]);
+  } while (stack.length > 0);
 }
--- workspaces/adventure-pack/goodies/typescript/traversePreOrder/index.ts
+++ workspaces/adventure-pack/goodies/typescript/traversePreOrder/index.ts
@@ -2,11 +2,13 @@ export function* traversePreOrder<
   T extends { left?: T | null | undefined; right?: T | null | undefined },
 >(root: T | null | undefined): Generator<T, void, void> {
   const stack = [root];
-  while (stack.length > 0) {
+
+  do {
     const node = stack.pop();
-    if (node) {
+
+    if (node != null) {
       yield node;
       stack.push(node.right, node.left);
     }
-  }
+  } while (stack.length > 0);
 }
