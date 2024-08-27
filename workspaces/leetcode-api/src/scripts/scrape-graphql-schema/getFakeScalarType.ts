e889618dfa60fcc3d32701e1fd25d7cefd3842b2
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-08-26 23:45:19 -0700

    LeetCode GraphQL: print more default values (#384)
    A bit hacked together, but it works!

---
+++ workspaces/leetcode-api/src/scripts/scrape-graphql-schema/getFakeScalarType.ts
@@ -0,0 +1,24 @@
+import {
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLScalarType,
+  type GraphQLInputType,
+  type GraphQLOutputType,
+} from "graphql";
+import nullthrows from "nullthrows";
+
+import type { InnerType } from "../../fetchGraphQLTypeInformation";
+
+export function getFakeScalarType(
+  innerType: InnerType,
+): GraphQLInputType & GraphQLOutputType {
+  if (innerType.kind === "LIST") {
+    return new GraphQLList(getFakeScalarType(nullthrows(innerType.ofType)));
+  }
+
+  if (innerType.kind === "NON_NULL") {
+    return new GraphQLNonNull(getFakeScalarType(nullthrows(innerType.ofType)));
+  }
+
+  return new GraphQLScalarType({ name: nullthrows(innerType.name) });
+}
