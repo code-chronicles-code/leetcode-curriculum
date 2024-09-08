d8fcc4355ad7af201f21ba05dea9627d724c1460
Raymond Broome <rbroome@gmail.com>
2024-09-08 09:49:38 -0400

    chore: use vars in test cases to avoid excessive nesting & run yarn format

--- workspaces/util/src/__tests__/distinctArray-test.ts
+++ workspaces/util/src/__tests__/distinctArray-test.ts
@@ -7,62 +7,64 @@ describe("distinctArray", () => {
   });
 
   it("can handle NaN in an iterable of numbers", () => {
-    expect(distinctArray([156, 89, NaN, 99, NaN, 156])).toStrictEqual([
-      156,
-      89,
-      NaN,
-      99,
-    ]);
+    const arr = [156, 89, NaN, 99, NaN, 156];
+    const expected = [156, 89, NaN, 99];
+
+    expect(distinctArray(arr)).toStrictEqual(expected);
   });
 
   it("can handle an iterable of booleans", () => {
-    expect(distinctArray([true, true, false, false])).toStrictEqual([
-      true,
-      false,
-    ]);
+    const arr = [true, true, false, false];
+    const expected = [true, false];
+
+    expect(distinctArray(arr)).toStrictEqual(expected);
   });
 
   it("can handle iterables like sets", () => {
-    expect(
-      distinctArray(
-        new Set(["uno", "dos", "tres", "cuatro", "cinco", "cinco", "seis"]),
-      ),
-    ).toStrictEqual(["uno", "dos", "tres", "cuatro", "cinco", "seis"]);
+    const iter = new Set([
+      "uno",
+      "dos",
+      "tres",
+      "cuatro",
+      "cinco",
+      "cinco",
+      "seis",
+    ]);
+    const expected = ["uno", "dos", "tres", "cuatro", "cinco", "seis"];
+
+    expect(distinctArray(iter)).toStrictEqual(expected);
   });
 
   it("defaults to comparing objects by reference", () => {
-    const obj1 = {id: "001"};
-    const map = new Map<{id: string}, string>([
-        [obj1, "object 1"],
-        [{id: "O02"}, "object 2"],
-        [{id: "O03"}, "object 3"],
-        [obj1, "object 1"],
-        [{id: "O04"}, "object 4"],
-        [obj1, "object 1"],
+    const obj1 = { id: "001" };
+    const map = new Map<{ id: string }, string>([
+      [obj1, "object 1"],
+      [{ id: "O02" }, "object 2"],
+      [{ id: "O03" }, "object 3"],
+      [obj1, "object 1"],
+      [{ id: "O04" }, "object 4"],
+      [obj1, "object 1"],
     ]);
-    
+
     const keys = Array.from(map.keys());
     expect(distinctArray(map)).toStrictEqual([
-        [keys[0], "object 1"],
-        [keys[1], "object 2"],
-        [keys[2], "object 3"],
-        [keys[3], "object 4"]
+      [keys[0], "object 1"],
+      [keys[1], "object 2"],
+      [keys[2], "object 3"],
+      [keys[3], "object 4"],
     ]);
   });
 
   it("is guaranteed to receive distinct result for a map with a keyFn", () => {
-    expect(
-      distinctArray(
-        new Map([
-          [1, "uno"],
-          [2, "dos"],
-          [3, "tres"],
-          [4, "tres"],
-          [5, "cinco!"],
-        ]),
-        (val) => val[1],
-      ),
-    ).toStrictEqual([
+    const map = new Map([
+      [1, "uno"],
+      [2, "dos"],
+      [3, "tres"],
+      [4, "tres"],
+      [5, "cinco!"],
+    ]);
+
+    expect(distinctArray(map, (val) => val[1])).toStrictEqual([
       [1, "uno"],
       [2, "dos"],
       [4, "tres"],
@@ -71,17 +73,17 @@ describe("distinctArray", () => {
   });
 
   it("can handle iterables like objects", () => {
-    expect(
-      distinctArray({
-        *[Symbol.iterator]() {
-          yield "uno";
-          yield "dos";
-          yield "tres";
-          yield "tres";
-          yield "cinco!";
-        },
-      }),
-    ).toStrictEqual(["uno", "dos", "tres", "cinco!"]);
+    const obj = {
+      *[Symbol.iterator]() {
+        yield "uno";
+        yield "dos";
+        yield "tres";
+        yield "tres";
+        yield "cinco!";
+      },
+    };
+
+    expect(distinctArray(obj)).toStrictEqual(["uno", "dos", "tres", "cinco!"]);
   });
 
   it("can handle iterables like strings", () => {
@@ -89,17 +91,18 @@ describe("distinctArray", () => {
   });
 
   it("can take a keyFn to define distinctiveness", () => {
-    expect(
-      distinctArray(
-        [
-          { name: "Joe" },
-          { name: "Joe" },
-          { name: "Joseph" },
-          { name: "Joe" },
-          { name: "Frank" },
-        ],
-        (val) => val.name,
-      ),
-    ).toStrictEqual([{ name: "Joe" }, { name: "Joseph" }, { name: "Frank" }]);
+    const arr = [
+      { name: "Joe" },
+      { name: "Joe" },
+      { name: "Joseph" },
+      { name: "Joe" },
+      { name: "Frank" },
+    ];
+
+    expect(distinctArray(arr, (val) => val.name)).toStrictEqual([
+      { name: "Joe" },
+      { name: "Joseph" },
+      { name: "Frank" },
+    ]);
   });
 });
