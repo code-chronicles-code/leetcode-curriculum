c2f42c95d3882f72f9c105515f5c30efd07d6f28
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-09-09 22:53:46 -0700

    Support variable statements in TypeScript goody module declarations

--- workspaces/adventure-pack/src/app/sortTypeScriptModuleAndInterfaceDeclarations.ts
+++ workspaces/adventure-pack/src/app/sortTypeScriptModuleAndInterfaceDeclarations.ts
@@ -4,16 +4,33 @@ import { compareStringsCaseInsensitive } from "@code-chronicles/util/compareStri
 import { mapObjectValues } from "@code-chronicles/util/mapObjectValues";
 import { sortObjectKeysRecursive } from "@code-chronicles/util/sortObjectKeysRecursive";
 
+import type { GoodyModuleDeclaration } from "../scripts/package-goodies/typescript/extractModuleDeclarations";
+
 export function sortTypeScriptModuleAndInterfaceDeclarations(
-  moduleDeclarations: ReadonlyDeep<Record<string, Record<string, string[]>>>,
-): Record<string, Record<string, string[]>> {
+  moduleDeclarations: ReadonlyDeep<Record<string, GoodyModuleDeclaration>>,
+): Record<string, GoodyModuleDeclaration> {
   return sortObjectKeysRecursive(
-    mapObjectValues(moduleDeclarations, (interfaceDeclarations) =>
-      mapObjectValues(interfaceDeclarations, (codeSections) =>
-        [...codeSections].sort(compareStringsCaseInsensitive),
-      ),
+    mapObjectValues(
+      moduleDeclarations,
+      (
+        moduleDeclaration: ReadonlyDeep<GoodyModuleDeclaration>,
+      ): GoodyModuleDeclaration => ({
+        ...(moduleDeclaration.interfaces && {
+          interfaces: mapObjectValues(
+            moduleDeclaration.interfaces,
+            (codeSections) =>
+              [...codeSections].sort(compareStringsCaseInsensitive),
+          ),
+        }),
+
+        ...(moduleDeclaration.variables && {
+          variables: [...moduleDeclaration.variables].sort(
+            compareStringsCaseInsensitive,
+          ),
+        }),
+      }),
     ),
     compareStringsCaseInsensitive,
-    2,
+    3,
   );
 }
--- workspaces/adventure-pack/src/scripts/package-goodies/typescript/extractModuleDeclarations.ts
+++ workspaces/adventure-pack/src/scripts/package-goodies/typescript/extractModuleDeclarations.ts
@@ -1,12 +1,18 @@
 import { type SourceFile as TSSourceFile, SyntaxKind } from "ts-morph";
 
+import { invariantViolation } from "@code-chronicles/util/invariantViolation";
 import { sortTypeScriptModuleAndInterfaceDeclarations } from "../../../app/sortTypeScriptModuleAndInterfaceDeclarations";
 import { removeNode } from "./removeNode";
 
+export type GoodyModuleDeclaration = {
+  interfaces?: Record<string, string[]>;
+  variables?: string[];
+};
+
 export function extractModuleDeclarations(
   sourceFile: TSSourceFile,
-): Record<string, Record<string, string[]>> {
-  const res: Record<string, Record<string, string[]>> = {};
+): Record<string, GoodyModuleDeclaration> {
+  const res: Record<string, GoodyModuleDeclaration> = {};
 
   sourceFile.getModules().forEach((mod) => {
     mod
@@ -14,33 +20,48 @@ export function extractModuleDeclarations(
       .getChildSyntaxListOrThrow()
       .getChildren()
       .forEach((child) => {
-        if (child.getKind() === SyntaxKind.SingleLineCommentTrivia) {
-          return;
+        switch (child.getKind()) {
+          case SyntaxKind.SingleLineCommentTrivia: {
+            return;
+          }
+          case SyntaxKind.InterfaceDeclaration: {
+            const interfaceDecl = child.asKindOrThrow(
+              SyntaxKind.InterfaceDeclaration,
+            );
+
+            const interfaceName = interfaceDecl.getName();
+            const typeParameters = interfaceDecl
+              .getFirstChildByKind(SyntaxKind.TypeParameter)
+              ?.getParentSyntaxListOrThrow()
+              .getFullText();
+
+            const interfaceKey =
+              typeParameters == null
+                ? interfaceName
+                : `${interfaceName}<${typeParameters}>`;
+
+            (((res[mod.getName()] ??= {}).interfaces ??= {})[interfaceKey] ??=
+              []).push(
+              interfaceDecl
+                .getChildSyntaxListOrThrow()
+                .getFullText()
+                .replace(/^\n+/, "")
+                .replace(/\n+$/, ""),
+            );
+
+            return;
+          }
+          case SyntaxKind.VariableStatement: {
+            ((res[mod.getName()] ??= {}).variables ??= []).push(
+              child.getFullText().trim(),
+            );
+            return;
+          }
         }
 
-        const interfaceDecl = child.asKindOrThrow(
-          SyntaxKind.InterfaceDeclaration,
-          "Only interface declarations are currently supported in module declarations, got: " +
+        invariantViolation(
+          "Only interface declarations or variable statements are currently supported in module declarations, got: " +
             child.getKindName(),
-        );
-
-        const interfaceName = interfaceDecl.getName();
-        const typeParameters = interfaceDecl
-          .getFirstChildByKind(SyntaxKind.TypeParameter)
-          ?.getParentSyntaxListOrThrow()
-          .getFullText();
-
-        const interfaceKey =
-          typeParameters == null
-            ? interfaceName
-            : `${interfaceName}<${typeParameters}>`;
-
-        ((res[mod.getName()] ??= {})[interfaceKey] ??= []).push(
-          interfaceDecl
-            .getChildSyntaxListOrThrow()
-            .getFullText()
-            .replace(/^\n+/, "")
-            .replace(/\n+$/, ""),
         );
       });
 
