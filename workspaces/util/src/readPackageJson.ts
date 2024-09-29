341a1eb1b2ac744d2882d1929713bcfc96e5e524
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-09-29 00:12:39 -0700

    preserve ordering

--- workspaces/util/src/readPackageJson.ts
+++ workspaces/util/src/readPackageJson.ts
@@ -5,6 +5,7 @@ import { z } from "zod";
 
 const packageJsonZodType = z.object({
   scripts: z.record(z.string(), z.string()).optional(),
+  workspaces: z.array(z.string()).optional(),
 });
 
 export type PackageJson = z.infer<typeof packageJsonZodType>;
@@ -12,8 +13,14 @@ export type PackageJson = z.infer<typeof packageJsonZodType>;
 export async function readPackageJson(
   directory: string = ".",
 ): Promise<PackageJson> {
-  const text = await readFile(path.join(directory, "package.json"), {
-    encoding: "utf8",
-  });
-  return packageJsonZodType.passthrough().parse(JSON.parse(text));
+  const data = JSON.parse(
+    await readFile(path.join(directory, "package.json"), {
+      encoding: "utf8",
+    }),
+  );
+
+  // Using Zod for validation only, not for returning, to preserve the ordering
+  // of keys in the original data.
+  packageJsonZodType.parse(data);
+  return data;
 }
