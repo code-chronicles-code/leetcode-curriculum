6c057fdc2807adb3c95a4801f241df4830351a1e
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-10-06 22:04:34 -0700

    Exclude operation name from generated GraphQL query constants (#455)
    Save some bytes!

--- workspaces/leetcode-api/src/scripts/codegen/graphqlCodegenPlugin.ts
+++ workspaces/leetcode-api/src/scripts/codegen/graphqlCodegenPlugin.ts
@@ -6,15 +6,19 @@ import { z } from "zod";
 
 import { isObject } from "@code-chronicles/util/isObject";
 import { only } from "@code-chronicles/util/only";
+import { spliceString } from "@code-chronicles/util/spliceString";
 
-const operationNameZodType = z
-  .object({ value: z.string() })
-  .transform(({ value }) => value);
+const nonNegativeIntZodType = z.number().int().nonnegative();
+
+const operationNameZodType = z.object({
+  value: z.string(),
+  loc: z.object({ start: nonNegativeIntZodType, end: nonNegativeIntZodType }),
+});
 
 export const plugin: PluginFunction<{}> = function plugin(_schema, documents) {
   // Encode some assumptions as invariants, namely that there is a single query
   // operation in the file.
-  const { document, rawSDL: unminifiedGraphql } = only(documents);
+  const { document, rawSDL: unminifiedGraphQL } = only(documents);
   const definition = only(nullthrows(document).definitions);
   invariant(
     isObject(definition) &&
@@ -23,10 +27,26 @@ export const plugin: PluginFunction<{}> = function plugin(_schema, documents) {
     "Expected a query!",
   );
 
-  const minifiedGraphql = graphqlQueryCompress(nullthrows(unminifiedGraphql));
-  const operationName = operationNameZodType.parse(definition.name);
+  // Extract the operation name from the definition, as well as information
+  // about its location in the raw, unminified GraphQL.
+  const {
+    value: operationName,
+    loc: { start: operationNameStart, end: operationNameEnd },
+  } = operationNameZodType.parse(definition.name);
+  const operationNameLength = operationNameEnd - operationNameStart;
+  invariant(
+    operationName.length === operationNameLength,
+    "Operation name length mismatch!",
+  );
 
-  // TODO: strip out the operation name from the minified GraphQL
+  // Minify the GraphQL we use for the query.
+  const minifiedGraphQL = graphqlQueryCompress(
+    spliceString(
+      nullthrows(unminifiedGraphQL),
+      operationNameStart,
+      operationNameLength,
+    ),
+  );
 
   return {
     prepend: [
@@ -42,7 +62,7 @@ export const plugin: PluginFunction<{}> = function plugin(_schema, documents) {
         export type QueryVariables = Simplify<${operationName}QueryVariables>;
         export type Query = Simplify<${operationName}Query>;  
 
-        export const QUERY = ${JSON.stringify(minifiedGraphql)};
+        export const QUERY = ${JSON.stringify(minifiedGraphQL)};
 
         export function fetchGraphQL(variables: QueryVariables): Promise<Query> {
           return getGraphQLClient().request(QUERY, variables);
