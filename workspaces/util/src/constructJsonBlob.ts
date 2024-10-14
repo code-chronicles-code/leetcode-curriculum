1dd77cc0e0398e8d3e3eb4ed930bbf5808896c35
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-10-13 17:52:46 -0700

    update a variable name

--- workspaces/util/src/constructJsonBlob.ts
+++ workspaces/util/src/constructJsonBlob.ts
@@ -1,7 +1,7 @@
 import type { JsonValue } from "type-fest";
 
-export const BLOB_MIME_TYPE = "application/json";
+export const JSON_MIME_TYPE = "application/json";
 
 export function constructJsonBlob(data: JsonValue): Blob {
-  return new Blob([JSON.stringify(data)], { type: BLOB_MIME_TYPE });
+  return new Blob([JSON.stringify(data)], { type: JSON_MIME_TYPE });
 }
