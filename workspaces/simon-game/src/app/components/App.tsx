6ac735e9a29e6a8cafa4d4fb3a3425f2df13078c
DominicS99 <dominic.sagen@gmail.com>
2024-10-24 22:07:20 -0700

    Early App update: Hello World --> 3 boxes

--- workspaces/simon-game/src/app/components/App.tsx
+++ workspaces/simon-game/src/app/components/App.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <div
+        style={{ backgroundColor: "lightblue", height: 100, width: 100 }}
+      ></div>
+      <div style={{ backgroundColor: "orange", height: 100, width: 100 }}></div>
+      <div style={{ backgroundColor: "green", height: 100, width: 100 }}></div>
+    </div>
+  );
 }
