6e382cd467c5652f0fe0178353c51701d8373c12
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-10-26 22:53:50 -0700

    wip

--- workspaces/adventure-pack/goodies/typescript/Iterator.prototype.flatMap/index.ts
+++ workspaces/adventure-pack/goodies/typescript/Iterator.prototype.flatMap/index.ts
@@ -1,3 +1,4 @@
+import "../Iterator.from/index.ts";
 import "../Iterator.prototype.toIterable/index.ts";
 import { iteratorPrototype } from "../Iterator.prototype/index.ts";
 
@@ -19,13 +20,7 @@ iteratorPrototype.flatMap ??= function* <TIn, TOut>(
 ): Generator<TOut, void, undefined> {
   let index = 0;
   for (const element of this.toIterable()) {
-    const res = callbackFn(element, index);
-    if (!(Symbol.iterator in res || typeof res.next === "function")) {
-      throw new TypeError("Callback must return an iterable or iterator");
-    }
-    yield* Symbol.iterator in res
-      ? (res as Iterable<TOut>)
-      : (res as Iterator<TOut>).toIterable();
+    yield* Iterator.from(callbackFn(element, index)).toIterable();
     ++index;
   }
 };
