d1feaeec52104fc776d332a29fe23ed92409d3c9
Eli Manzo <96manzo.eli@gmail.com>
2024-10-27 22:15:29 -0700

    chore: added some error handling

--- workspaces/simon-game/src/app/util/playSound.ts
+++ workspaces/simon-game/src/app/util/playSound.ts
@@ -1,9 +1,29 @@
+/**
+ * Plays a sound using the Web Audio API.
+ *
+ * @param context - The audio context to use for generating the sound.
+ * @param frequency - The frequency of the sound in Hertz
+ * @param duration - The duration of the sound in milliseconds.
+ * @param volume - The volume of the sound, typically between 0 and 1.
+ */
 export const playSound = (
   context: BaseAudioContext,
   frequency: number,
   duration: number,
   volume: number,
 ) => {
+  if (frequency <= 0) {
+    throw new Error("Frequency must be a positive number.");
+  }
+
+  if (duration <= 0) {
+    throw new Error("Duration must be a positive number.");
+  }
+
+  if (volume < 0 || volume > 1) {
+    throw new Error("Volume must be between 0 and 1.");
+  }
+
   const oscillatorNode = context.createOscillator();
   oscillatorNode.frequency.setValueAtTime(frequency, context.currentTime);
 
