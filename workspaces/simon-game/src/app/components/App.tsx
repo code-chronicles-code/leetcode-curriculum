eefe6ad2727fbe71f3a823bd203089eb6a742d20
Eli Manzo <96manzo.eli@gmail.com>
2024-10-28 21:24:19 -0700

    chore: applying feedback

--- workspaces/simon-game/src/app/components/App.tsx
+++ workspaces/simon-game/src/app/components/App.tsx
@@ -6,25 +6,19 @@ import { Box } from "./Box.tsx";
 
 export function App() {
   let audioContext: AudioContext | null = null;
-
-  const handleFreqClick = (freq: number) => {
-    if(!audioContext) {
-      audioContext = new window.AudioContext();
-    }
-    playSound(
-      audioContext,
-      freq,
-      buttonSoundConfig.duration,
-      buttonSoundConfig.volume,
-    )
-  }
-
   return (
     <div>
       {buttonSoundConfig.frequencies.map((freq, index) => (
         <button
           key={index}
-          onClick={() => handleFreqClick(freq)}
+          onClick={() =>
+            playSound(
+              (audioContext ??= new AudioContext()),
+              freq,
+              buttonSoundConfig.duration,
+              buttonSoundConfig.volume,
+            )
+          }
         >
           {freq}
         </button>
