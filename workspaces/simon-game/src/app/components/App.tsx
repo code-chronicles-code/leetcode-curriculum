419524848d7b3f88ced1ae51985372ba12bd7882
Eli Manzo <96manzo.eli@gmail.com>
2024-11-02 00:59:32 -0700

    feat: added a list to keep track of player moves

--- workspaces/simon-game/src/app/components/App.tsx
+++ workspaces/simon-game/src/app/components/App.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Box } from "./Box.tsx";
 import { playNote } from "../util/playNote.ts";
 import { config } from "../constants.ts";
 
 export function App() {
+  const [playerMoves, setPlayerMoves] = useState<number[]>([]);
   return (
     <div style={{ display: "flex", gap: 10 }}>
-      {config.boxes.map((box) => (
-        <Box color={box.color} onClick={() => playNote(box.frequency)} />
+      {config.boxes.map((box, index) => (
+        <Box
+          color={box.color}
+          onClick={() => {
+            playNote(box.frequency);
+            setPlayerMoves([...playerMoves, index]);
+          }}
+        />
       ))}
     </div>
   );
