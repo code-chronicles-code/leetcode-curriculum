dd1e70d756e19e1d755780c572e676640163875e
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-11-03 20:11:18 -0800

    Simplify PotD bot messages

--- workspaces/post-leetcode-potd-to-discord/src/getPotdMessage.ts
+++ workspaces/post-leetcode-potd-to-discord/src/getPotdMessage.ts
@@ -1,38 +1,21 @@
+import dedent from "dedent";
+
 import type { ActiveDailyCodingChallengeQuestion } from "@code-chronicles/leetcode-api/active-daily-coding-challenge-question";
 import { SEC_IN_DAY } from "@code-chronicles/util/timeConstants";
 import { yearMonthDayToTimestampInSeconds } from "@code-chronicles/util/yearMonthDayToTimestampInSeconds";
 
 import { formatTimestampForDiscord } from "./formatTimestampForDiscord.ts";
 
+// TODO: jest tests
 export function getPotdMessage({
   date,
   question: { difficulty, questionFrontendId, title, titleSlug },
 }: ActiveDailyCodingChallengeQuestion): string {
   const link = `https://leetcode.com/problems/${titleSlug}/`;
-  const sentences = [
-    `✨ New LeetCode problem of the day: [${questionFrontendId}. ${title}](${link}) ✨`,
-  ];
 
-  switch (difficulty) {
-    case "Easy": {
-      sentences.push("It's marked easy, so don't overthink it!");
-      break;
-    }
-    case "Medium": {
-      sentences.push("It's marked medium, so nothing too scary.");
-      break;
-    }
-    case "Hard": {
-      sentences.push(
-        "It's marked hard, but that's just a label. Ask for help here if you need it!",
-      );
-      break;
-    }
-  }
+  return dedent`
+    ✨ New LeetCode problem of the day: [${questionFrontendId}. ${title}](${link}) (marked ${difficulty}) ✨
 
-  sentences.push(
-    `\n-# Problem due - ⏳ ****${formatTimestampForDiscord(yearMonthDayToTimestampInSeconds(date) + SEC_IN_DAY, "R")}**** ⏳`,
-  );
-
-  return sentences.join("\n");
+    -# Problem due: ⏳ **${formatTimestampForDiscord(yearMonthDayToTimestampInSeconds(date) + SEC_IN_DAY, "R")}** ⏳
+  `;
 }
