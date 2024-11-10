56f3e4ad9fda074ec76af8a2b40bf97655ce2d93
Miorel-Lucian Palii <mlpalii@gmail.com>
2024-11-09 19:21:47 -0800

    Respect undo/redo stack in LeetCode Prettier extension
    The previous method of editing the editor content would destroy the history of the user's edits. This doesn't!

--- workspaces/leetcode-prettier-extension/src/main.ts
+++ workspaces/leetcode-prettier-extension/src/main.ts
@@ -1,6 +1,9 @@
+import nullthrows from "nullthrows";
 import { format } from "prettier/standalone";
 import estreePlugin from "prettier/plugins/estree";
 import tsPlugin from "prettier/plugins/typescript";
+
+import { assignFunctionCosmeticProperties } from "@code-chronicles/util/object-properties/assignFunctionCosmeticProperties";
 
 import { isMonaco } from "./isMonaco.ts";
 
@@ -22,44 +25,55 @@ function main(): void {
       }
 
       monaco.editor.onDidCreateEditor((ed) => {
-        ed
-          ?.getModel?.()
-          ?.updateOptions?.({ tabSize: 2, indentSize: "tabSize" });
+        try {
+          nullthrows(ed.getModel()).updateOptions({
+            tabSize: 2,
+            indentSize: "tabSize",
+          });
+        } catch (err) {
+          console.error(err);
+        }
 
         if (typeof ed?.getAction !== "function") {
-          // TODO: console.error something interesting
+          console.error("Monaco editor doesn't have a `getAction` method!");
           return;
         }
 
         const { getAction } = ed;
-
-        ed.getAction = function (this: unknown) {
+        ed.getAction = assignFunctionCosmeticProperties(function (
+          this: unknown,
+        ) {
           const action = getAction.apply(
             this,
             // Slight lie but `.apply` will work with the `arguments` object.
             arguments as unknown as Parameters<typeof getAction>,
           );
-          if (!action) {
-            // TODO: console.error something interesting
+
+          if (typeof action?.run !== "function") {
+            console.error("Monaco action object doesn't have a `run` method!");
             return action;
           }
 
-          action.run = async function () {
+          action.run = assignFunctionCosmeticProperties(async function () {
             try {
               const formattedText = await format(ed.getValue(), {
                 parser: "typescript",
                 plugins: [estreePlugin, tsPlugin],
               });
 
-              // TODO: switch to https://microsoft.github.io/monaco-editor/typedoc/interfaces/editor.ITextModel.html#pushEditOperations.pushEditOperations-1 in the future
-              ed.setValue(formattedText);
+              ed.executeEdits("Prettier", [
+                {
+                  range: nullthrows(ed.getModel()).getFullModelRange(),
+                  text: formattedText,
+                },
+              ]);
             } catch (err) {
               console.error(err);
             }
-          };
+          }, action.run);
 
           return action;
-        };
+        }, getAction);
       });
     },
   });
