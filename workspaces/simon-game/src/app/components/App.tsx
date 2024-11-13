a315cef01573d0d9bd2cd687392aeb263618097f
DominicS99 <98355617+DominicS99@users.noreply.github.com>
2024-11-12 21:29:16 -0800

    Adding GameState enum and barebones pre-game screen (#500)

--- workspaces/simon-game/src/app/components/App.tsx
+++ workspaces/simon-game/src/app/components/App.tsx
@@ -4,8 +4,32 @@ import { Box } from "./Box.tsx";
 import { playNote } from "../util/playNote.ts";
 import { config } from "../constants.ts";
 
+type GameState = "pre-game" | "game-over" | "player-turn" | "cpu-turn";
+
 export function App() {
   const [playerMoves, setPlayerMoves] = useState<readonly number[]>([]);
+  const [gameState, setGameState] = useState<GameState>("pre-game");
+  const [_correctMoves, _setCorrectMoves] = useState<readonly number[]>([
+    0, 1, 2, 3,
+  ]);
+
+  console.log(_correctMoves, _setCorrectMoves);
+
+  if (gameState === "pre-game") {
+    return (
+      <div style={{ display: "flex", gap: 10 }}>
+        <button
+          onClick={() => {
+            setGameState("cpu-turn");
+          }}
+        >
+          Start Game
+        </button>
+        Simon Game
+      </div>
+    );
+  }
+
   return (
     <>
       <div style={{ display: "flex", gap: 10 }}>
